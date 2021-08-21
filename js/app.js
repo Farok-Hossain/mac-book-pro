d576a2fac65d55946a55c3912ecf269612024714
Farok-Hossain <farokafs7@gmail.comm>
2021-08-21 19:09:52 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,4 @@
+document.getElementById('16gb-memory').addEventListener('click', function(){
+    const memeryCost = document.getElementById('extra-memory-cost');
+    const exMemoryCost = memeryCost.innerText + 180;
+})
