36f9344b78768336af38ca218f5e3acb7c26d895
Farok-Hossain <farokafs7@gmail.comm>
2021-08-21 19:11:57 +0600

    add

--- js/app.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('16gb-memory').addEventListener('click', function(){
-    const memeryCost = document.getElementById('extra-memory-cost');
-    const exMemoryCost = memeryCost.innerText + 180;
-})
